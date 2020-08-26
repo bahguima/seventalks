cd1d397a7722abd680c574af9e6cadea58435e40
bah <barbaraguieiro@gmail.com>
2020-08-26 11:43:00 -0300

    mostrar pop up só uma vez

--- js/popup.js
+++ js/popup.js
@@ -1,6 +1,8 @@
+
+let ja_passou = "0";
 function onMouseOut(event) {
 
-    if (
+    if ( ja_passou = "0" &&
       event.clientY < 50 &&
       event.relatedTarget == null &&
       event.target.nodeName.toLowerCase() !== 'select') {
@@ -9,8 +11,18 @@ function onMouseOut(event) {
   
       // Mostra o pop up
       document.getElementById("popup").style.display = "block";
+      ja_passou="1";
+
     }
   }
   
   document.addEventListener("mouseout", onMouseOut);
+ 
+  const exit = e => {
+    if (e.target.className === 'close') {
+      document.getElementById("popup").style.display = "none";
+    }
+};
+
+document.querySelector('.popup__wrapper').addEventListener('click', exit);
 
